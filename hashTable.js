ce1d5e947ab65b46f31b9de578285a3d19775712
Dewa Bisma <idewagedebisma@gmail.com>
2021-06-22 09:59:11 +0800

    hash table set, get, and keys

--- hashTable.js
+++ hashTable.js
@@ -1,31 +1,60 @@
 "use strict";
 var HashTable = /** @class */ (function () {
-  function HashTable(size) {
-    if (size === void 0) {
-      size = 29;
+    function HashTable(size) {
+        if (size === void 0) { size = 29; }
+        this.dataKeys = [];
+        this.dataMap = new Array(size);
     }
-    this.dataMap = new Array(size);
-  }
-  HashTable.prototype._hash = function (key) {
-    var hash = 0;
-    for (var i = 0; i < key.length; i++) {
-      var ascii = key.charCodeAt(i);
-      hash = (hash + ascii * 103) % this.dataMap.length;
-    }
-    return hash;
-  };
-  HashTable.prototype.set = function (key, val) {
-    var index = this._hash(key);
-    if (!this.dataMap[index]) {
-      this.dataMap[index] = [];
-    }
-    this.dataMap[index].push([key, val]);
-    return this;
-  };
-  return HashTable;
-})();
+    HashTable.prototype._hash = function (key) {
+        var hash = 0;
+        for (var i = 0; i < key.length; i++) {
+            var ascii = key.charCodeAt(i);
+            hash = (hash + ascii * 103) % this.dataMap.length;
+        }
+        return hash;
+    };
+    HashTable.prototype.set = function (key, val) {
+        var index = this._hash(key);
+        if (!this.dataMap[index]) {
+            this.dataMap[index] = [];
+        }
+        this.dataMap[index].push([key, val]);
+        this.dataKeys.push(key);
+        return this;
+    };
+    HashTable.prototype.get = function (key) {
+        var index = this._hash(key);
+        if (!this.dataMap[index]) {
+            return "Data not found!";
+        }
+        else {
+            var isExist_1 = null;
+            this.dataMap[index].forEach(function (data) {
+                if (data[0] === key)
+                    isExist_1 = data;
+            });
+            if (isExist_1) {
+                return isExist_1[1];
+            }
+            else {
+                return "Data not found!";
+            }
+        }
+    };
+    HashTable.prototype.keys = function () {
+        if (this.dataKeys.length > 0) {
+            return this.dataKeys;
+        }
+        else {
+            return "HashTable is empty";
+        }
+    };
+    return HashTable;
+}());
 var hashTable = new HashTable();
-
 console.log(hashTable.set("wayan", 1234));
 console.log(hashTable.set("ketut", 9999));
+console.log(hashTable.get("wayan"));
+console.log(hashTable.get("gusti"));
+console.log(hashTable.keys());
 console.log(hashTable);
--- hashTable.ts
+++ hashTable.ts
@@ -1,11 +1,12 @@
 class HashTable<T> {
   public dataMap: [string, T][][];
+  private dataKeys: string[] = [];
 
   constructor(size: number = 29) {
     this.dataMap = new Array(size);
   }
 
-  _hash(key: string) {
+  private _hash(key: string) {
     let hash = 0;
 
     for (let i = 0; i < key.length; i++) {
@@ -24,14 +25,64 @@ class HashTable<T> {
     }
 
     this.dataMap[index].push([key, val]);
+    this.dataKeys.push(key);
 
     return this;
   }
+
+  get(key: string) {
+    const index = this._hash(key);
+
+    if (!this.dataMap[index]) {
+      return "Data not found!";
+    } else {
+      let isExist = null;
+
+      this.dataMap[index].forEach((data) => {
+        if (data[0] === key) isExist = data;
+      });
+
+      if (isExist) {
+        return isExist[1];
+      } else {
+        return "Data not found!";
+      }
+    }
+  }
+
+  keys() {
+    if (this.dataKeys.length > 0) {
+      return this.dataKeys;
+    } else {
+      return "HashTable is empty";
+    }
+  }
+
+  // keys() {
+  //   let keys: string[] = [];
+
+  //   if (!this.dataMap) {
+  //     return "HashTable is empty";
+  //   } else {
+  //     this.dataMap.forEach((data) => {
+  //       data.forEach((pair) => {
+  //         keys.push(pair[0]);
+  //       });
+  //     });
+
+  //     return keys;
+  //   }
+  // }
 }
 
 const hashTable = new HashTable<number>();
 
 console.log(hashTable.set("wayan", 1234));
 console.log(hashTable.set("ketut", 9999));
+
+console.log(hashTable.get("wayan"));
+console.log(hashTable.get("gusti"));
+
+console.log(hashTable.keys());
 
 console.log(hashTable);
